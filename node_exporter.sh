7cfc35fd3fdeef96207eba122f0fe4de7f2e4f6e
kocercan <65725644+kocercan@users.noreply.github.com>
2021-06-16 23:25:31 +0300

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo useradd --no-create-home --shell /bin/false node_exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz 
 tar xvf node_exporter-1.1.2.linux-amd64.tar.gz  
 sudo cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin 
